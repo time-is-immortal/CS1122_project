57be8f829ed9bc9075acb10d1d1989802073658a
jtl385 <jtl385@nyu.edu>
2016-04-20 19:46:15 -0400

    Update Bullet.py

--- Bullet.py
+++ Bullet.py
@@ -1,16 +1,18 @@
 import pygame
+import math
 
 class Bullet:
-  def __init__(self, x_coord, y_coord): #x and y coords should just be player's coords, since it is shot out of the player
+  def __init__(self, x_coord, y_coord, angle): #x and y coords should just be player's coords, since it is shot out of the player
     self.x_coord = x_coord
     self.y_coord = y_coord
-    velocity = (5, 5) #placeholder, later we make the direction according to the mouse's aim
+    self.angle = angle #angle needs to be calculated by the player when it calls shootBullet()
+    self.speed = (5*math.cos(self.angle), 5*math.sin(self.angle))
     
   def drawUpdate(self, gameDisplay): #moves the bullet according to velocity, then draws it
     if self.x_coord < 800 and self.y_coord < 600: #checks if bullet is off screen
-      self.x_coord += velocity[0]
-      self.y_coord += velocity[1]
+      self.x_coord += self.speed[0]
+      self.y_coord += self.speed[1]
       pygame.draw.circle(gameDisplay, Color.black, [x_coord, y_coord, 10, 10], 3)
     else:
-      pass
+      self.kill()
     
