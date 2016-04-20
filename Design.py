f1d7dacf0c51da0db4653645ecd199a313692604
Robe Zhang <rzhang8277@bths.edu>
2016-04-20 17:20:23 -0400

    Borders

---
+++ Design.py
@@ -0,0 +1,15 @@
+
+#define colors    
+class Color:
+    white = (255,255,255)
+    black = (0,0,0)
+    red = (255,0,0)
+    green = (0,255,0)
+    blue = (0,0,255)
+    grayish = (192,192,192)
+
+#define borders
+class Layout:
+    screen_width = 800
+    screen_height = 600
+    borderOffset = 5
