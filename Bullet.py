28dbab4eb513e63f02be0e0610bcc811bb380960
jtl385 <jtl385@nyu.edu>
2016-04-20 22:59:21 -0400

    Update Bullet.py

--- Bullet.py
+++ Bullet.py
@@ -10,9 +10,9 @@ class Bullet:
   def __init__(self, x_coord, y_coord, angle): #x and y coords should just be player's coords, since it is shot out of the player
     self.x_coord = x_coord
     self.y_coord = y_coord
-    # self.angle = angle #angle needs to be calculated by the player when it calls shootBullet()
-    # self.speed = (5*math.cos(self.angle), 5*math.sin(self.angle))
-    self.speed = (1,1)
+    self.angle = angle #angle needs to be calculated by the player when it calls shootBullet()
+    self.speed = (15*math.cos(self.angle), 15*math.sin(self.angle))
+    # self.speed = (1,1)
     self.isOffScreen = False
     
   def drawUpdate(self, gameDisplay): #moves the bullet according to velocity, then draws it
